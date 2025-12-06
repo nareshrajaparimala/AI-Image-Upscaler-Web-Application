78a04d59fc6106de572c62200d481f89cae8c060
Naresh.R <nareshrajaparimala000@gmail.com>
2025-12-06 23:57:08 +0530

    The deployment stage 2

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), { canvas: 'canvas' }];
-    return config;
-  },
+  turbopack: {},
   images: {
     remotePatterns: [
       {
